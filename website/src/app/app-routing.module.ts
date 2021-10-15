3a30a4bb05a0a69861dd3c49182722e847d2aabf
tony dandria <tony.dandria@hotmail.com>
2021-10-15 11:26:17 +0200

    wip

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {FrontPageComponent} from "./pages/front-page/front-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: FrontPageComponent},
+  // {path: '**', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './pages/front-page/front-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
